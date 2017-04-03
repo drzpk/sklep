7413391d19a1200d92d39c18763f8a7e35525ac0
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2017-04-03 21:04:20 +0200

    Zmiana struktury bazy danych pod przyszłą aktualizację strony

--- db_init.sql
+++ db_init.sql
@@ -4,35 +4,54 @@ USE sklep;
 
 DROP TABLE IF EXISTS towary;
 DROP TABLE IF EXISTS kategorie;
+DROP TABLE IF EXISTS users;
 
+#KATEGORIE
 CREATE TABLE IF NOT EXISTS kategorie (
 	id_kategoria INT(11) NOT NULL AUTO_INCREMENT,
 	nazwa VARCHAR(64) NOT NULL,
 	PRIMARY KEY(id_kategoria)
 );
 
+#TOWARY
 CREATE TABLE IF NOT EXISTS towary (
 	id_towar INT(11) NOT NULL AUTO_INCREMENT,
 	nazwa VARCHAR(64) NOT NULL,
+	opis VARCHAR(256), 
 	cena INT(11) NOT NULL,
 	id_kategoria INT(11) NOT NULL,
+	image BLOB,
 	PRIMARY KEY(id_towar),
 	FOREIGN KEY(id_kategoria) REFERENCES kategorie(id_kategoria)
 );
 
+#UŻYTKOWNICY
+CREATE TABLE IF NOT EXISTS users (
+	id_user INT(11) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(32) NOT NULL,
+	password CHAR(64) NOT NULL,
+	admin BOOLEAN DEFAULT FALSE,
+	PRIMARY KEY(id_user)
+);
+
+SET NAMES UTF8;
+
+#DODANIE PRZYKŁADOWYCH KATEGORII
 INSERT IGNORE INTO kategorie VALUES 
 	(1, 'procesory'),
 	(2, 'płyty główne'),
 	(3, 'pamięci RAM'),
 	(4, 'dyski twarde');
 
-INSERT IGNORE INTO towary VALUES 
+#DODANIE PRZYKŁADOWYCH TOWARÓW
+INSERT IGNORE INTO towary (id_towar, nazwa, cena, id_kategoria) VALUES 
 	(1, 'Intel Core i5-6300', 990, 1),
 	(2, 'Gigabyte HN37473-USB3', 350, 2),
 	(3, 'Intel Pentium G630', 199, 1),
 	(4, 'Seagate Barracuda 3GGJ382', 245, 4),
 	(5, 'WD Black 3872KM.2', 450, 4),
 	(6, 'Corsair 4GBJW8374', 200, 3);
-	
-	
--- SELECT t.id_towar AS '1', t.nazwa AS '2', t.cena AS '3', k.nazwa AS '4' FROM towary AS t JOIN kategorie AS k ON t.id_kategoria=k.id_kategoria ORDER BY k.nazwa;
+
+#DODANIE DOMYŚLNEGO UŻYTKOWNIKA (hasło: root)
+INSERT IGNORE INTO users VALUES 
+	(1, 'root', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2', TRUE);
