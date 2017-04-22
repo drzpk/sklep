37e8364912e2f26c032b31e7f8f89e2e4f5e54fc
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2017-04-22 18:29:08 +0200

    Zmiana wyglądu wyświetlanych przedmiotów

--- list_view.php
+++ list_view.php
@@ -3,16 +3,25 @@ include 'skeleton.php';
 
 class ListView extends Skeleton {
 	
+	//liczba elementów na stronę
+	const ELEMENTS_PER_PAGE = 6;
+	
+	/** gotowe zapytanie */
+	private $query;
+	/** lista kategorii */
+	private $cateogies;
+
+	/** obecna strona, liczone od 1 */
+	private $current_page;
+	private $total_pages;
+	private $has_next_page;
+	
 	/*
 		parametr sort:
 		1 - według nazwy
 		2 - według numeru
 		3 - według kategorii
 	*/
-	private $is_result = false;
-	private $error_msg = false;
-	private $result = array();
-	
 	private $sort = -1;
 	private $filter = -1;
 	
@@ -20,19 +29,78 @@ class ListView extends Skeleton {
 		$this->init_db();
 		
 		if (isset($_REQUEST['sort'])) {
+			//parametr sortowania
 			$this->sort = (int) $_REQUEST['sort'];
 		}
 
 		if (isset($_REQUEST['filter'])) {
+			//parametr filtrowania
 			$this->filter = (int) $_REQUEST['filter'];
 		}
 			
+		//przygotowanie zapytania
 		if ($this->sort >= -1 && $this->sort < 3) {
-			$this->create_result();
+			$q_str = "SELECT t.id_towar, t.nazwa, t.opis, t.cena, k.nazwa, t.image"
+						. " FROM towary AS t JOIN kategorie AS k ON t.id_kategoria=k.id_kategoria";
+		
+			if ($this->filter > -1) {
+				$q_str .= " WHERE k.id_kategoria={$this->filter} ";
+			}
+		
+			$q_str .= " ORDER BY ";
+			switch ($this->sort) {
+				case -1:
+					$q_str .= 't.id_towar';
+					break;
+				case 0:
+					$q_str .= 't.nazwa';
+					break;
+				case 1:
+					$q_str .= 't.id_towar';
+					break;
+				case 2:
+					$q_str .= 'k.nazwa';
+				
+			}
+			
+			//zapisanie zapytania
+			$this->query = $q_str;
 		}
 		else {
-			$error_msg = 'Parametry są filtrowane, powodzenia!';
+			$his->error('Podano nieprawidłowy parametr sortowania.');
+			return;
 		}
+
+		//określenie obecnej strony
+		if (isset($_REQUEST['page'])) {
+			$page = (int) $_REQUEST['page'];
+		}
+		else
+			$page = 1;
+
+		$result = $this->db->query('SELECT COUNT(*) FROM towary');
+		$pages = ceil($result->fetch_array()[0] / self::ELEMENTS_PER_PAGE);
+		$result->close();
+
+		if ($page < 1 || $page > $pages) {
+			$this->error('Nieprawidłowy numer strony!');
+			return;
+		}
+
+		$this->current_page = $page;
+		$this->total_pages = $pages;
+		$this->has_next_page = $pages - $page > 0;
+
+		$offset = ($page - 1) * self::ELEMENTS_PER_PAGE;
+		$this->query .= ' LIMIT ' . ((string) $offset);
+		$this->query .= ', ' . ((string) self::ELEMENTS_PER_PAGE);
+		
+		//pobranie listy kategorii
+		$result = $this->db->query('SELECT * FROM kategorie');
+		while ($row = $result->fetch_array()) {
+			$this->categories[$row[0]] = $row[1];
+		}
+		$result->close();
 	}
 	
 	public function getTitle() {
@@ -60,21 +128,17 @@ class ListView extends Skeleton {
 	?>
 	</fieldset>
 	<br>
-	<?php
-	$rs = $this->db->query('SELECT * FROM kategorie');
-	?>
 	<div class="input-field">
 		<select name="filter">
 			<option value="-1">Brak filtrowania</option>
 			<?php 
-			while ($row = $rs->fetch_row()) {
-				$str = "<option value=\"{$row[0]}\"";
-				if ($row[0] == $this->filter)
+			foreach ($this->categories as $k => $v) {
+				$str = "<option value=\"{$k}\"";
+				if ($k == $this->filter)
 					$str .= ' selected="selected"';
-				$str .= ">{$row[1]}</option>";
+				$str .= ">{$v}</option>";
 				echo $str;
 			}
-			$rs->free();
 			?>
 		</select>
 		<label>Filtrowanie</label>
@@ -82,63 +146,62 @@ class ListView extends Skeleton {
 	<br><input class="btn" type="submit" value="wyślij">
 </form>	
 <?php
-	}
+	} //drawNav
 
 	public function drawSection() {
-?>
-<?php if ($this->is_result) { ?>
-<table class="highlight">
-	<thead>
-		<tr>
-			<th>ID</th>
-			<th>Nazwa</th>
-			<th>Cena</th>
-			<th>Kategoria</th>
-		</tr>
-	</thead>
-	<tbody>
-	<?php foreach ($this->result as $row) : ?>
-		<tr>
-			<td><?php echo $row['1']; ?></td>
-			<td><?php echo $row['2']; ?></td>
-			<td><?php echo $row['3']; ?></td>
-			<td><?php echo $row['4']; ?></td>
-		</tr>
-	<?php endforeach; ?>
-	</tbody>
-</table>
-<?php
-}
-elseif ($this->error_msg)
-	$this->error($this->error_msg);
-else
-	$this->info("Wybierz jakąś opcję z menu nawigacji");
+		$result = $this->db->query($this->query);
+		$first = true;
+		while ($row = $result->fetch_array()) {
+			if (!$first)
+				echo '<hr>';
+			else
+				$first = false;
+
+			$this->displayRow($row);
+		}
+		$result->close();
+
+		$file = $_SERVER['PHP_SELF'];
+		echo '<br>';
+
+		if ($this->current_page > 1) {
+			$prev = $this->current_page - 1;
+			echo "<a class=\"btn-flat\" href=\"{$file}?page={$prev}\">Poprzednia strona</a>";
+		}
+		else
+			echo '<p class="disabled inactive btn-flat">Poprzednia strona</p>';
+
+		//TODO: wyśrodkować informację o stronie
+		echo '<p class="inline">Strona: ' . $this->current_page . '/' . $this->total_pages . '</p>';
+		
+		if ($this->has_next_page) {
+			$next = $this->current_page + 1;
+			echo "<a class=\"btn-flat right\" href=\"{$file}?page={$next}\">Następna strona</a>";
+		}
+		else
+			echo '<p class="disabled inactive btn-flat right">Następna strona</p>';
 	}
-	
-	private function create_result() {
-		$q_str = "SELECT t.id_towar AS '1', t.nazwa AS '2', t.cena AS '3', k.nazwa AS '4' FROM towary AS t JOIN kategorie AS k ON t.id_kategoria=k.id_kategoria";
-		
-		if ($this->filter > -1) {
-			$q_str .= " WHERE k.id_kategoria={$this->filter} ";
+
+	private function displayRow($row) {
+		echo '<div class="item-view">';
+		if ($row[5]) {
+			$data = 'data:image/png;base64,' . $row[5];
+			echo '<img src="' . $data . '" alt="zdjęcie przedmiotu">';
 		}
-		if ($this->sort > -1) {
-			$q_str .= " ORDER BY ";
-			switch ($this->sort) {
-				case 0:
-					$q_str .= 't.nazwa';
-					break;
-				case 1:
-					$q_str .= 't.id_towar';
-					break;
-				case 2:
-					$q_str .= 'k.nazwa';
-			}
-		}
-		
-		$q = $this->db->query($q_str);
-		while ($row = $q->fetch_assoc())
-			$this->result[] = $row;
-		$this->is_result = true;
+		else
+			echo '<img src="img/blank-image.png" alt="zdjęcie przedmiotu">';
+?>
+		<div>
+			<p><?php echo $row[1]; ?></p>
+			<p><?php echo $row[2]; ?></p>
+			<p><?php echo $row[4]; ?></p>
+			<div>
+				<p><?php echo $row[3] . 'zł'; ?></p>
+				<a class="btn" eid="<?php echo $row[0]; ?>"><i class="material-icons">shopping_card</i>Dodaj do koszyka</a>
+			</div>
+		</div>
+	</div>
+<?php
 	}
 }
 
