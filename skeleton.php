4468c5413288be457a3af15ac8fffb1ac4b15dc4
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2017-05-18 16:40:51 +0200

    Wyłącznie indeksowania przez roboty indeksujące

--- skeleton.php
+++ skeleton.php
@@ -110,6 +110,9 @@ abstract class Skeleton {
 	<head>
 		<meta charset="utf-8">
 		<title>Sklep komputerowy</title>
+
+		<meta name="robots" content="noindex, nofollow">
+
 		<link rel="stylesheet" href="css/materialize.min.css">
 		<link rel="stylesheet" href="css/style.css?v=49">
 		<link rel="stylesheet" href="css/icons.css">
